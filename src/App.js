ae47e26a17eb938d0ae2ae574dafb29131ce658d
EmilyRacheal <emilychealy4@gmail.com>
2022-10-03 15:18:29 +0100

    adding a table to get datas

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [data, storeDate] = useState([]);
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        console.log("Getting from ::::::::", res.data);
+        storeDate(res.data);
+      })
       .catch((err) => console.log(err));
   }, []);
+
+  const arr = data.map((data, index) => {
+    return (
+      <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+      </tr>
+    );
+  });
+
   return (
     <div className="App">
       <h1>Let's use axios with React js</h1>
+
+      <h2>A basic HTML table</h2>
+
+      <table>
+        <tr>
+          <th>Id</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+      </table>
+
+      <p>
+        To understand the example better, we have added borders to the table.
+      </p>
     </div>
   );
 }
