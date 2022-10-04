d738495933554b789502d07a07b450c5b1f04268
EmilyRacheal <emilychealy4@gmail.com>
2022-10-04 15:12:09 +0100

    using the useref to change the previous name to the current

--- src/Pages/Useref/index.jsx
+++ src/Pages/Useref/index.jsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useRef, useState } from "react";
 
 function Index() {
-  const [name, setName] = useState("");
+  // const [name, setName] = useState("");
 
-  const inputRef = useRef();
-  const focus = () => {
-    inputRef.current.focus();
-  };
+  // const inputRef = useRef();
+  // const focus = () => {
+  //   inputRef.current.focus();
+  // };
+
+  const [name, setName] = useState("");
+  const prevName = useRef("");
+
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
 
   return (
     <div>
-      <input
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <h1>
+        My name is {name} and it used to be {prevName.current}
+      </h1>
+
+      {/* <input
         ref={inputRef}
         value={name}
         onChange={(e) => setName(e.target.value)}
       />
       <h1>My name is {name}</h1>
-      <button onClick={focus}>focus the input</button>
+      <button onClick={focus}>focus the input</button> */}
     </div>
   );
 }
