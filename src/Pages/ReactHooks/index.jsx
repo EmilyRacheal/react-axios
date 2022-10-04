0dd46dc1ecae00edcafddaf3499c1c13767df9aa
EmilyRacheal <emilychealy4@gmail.com>
2022-10-04 11:36:24 +0100

    using the useEffect hooks

--- src/Pages/ReactHooks/index.jsx
+++ src/Pages/ReactHooks/index.jsx
@@ -3,12 +3,23 @@ import { useState, useEffect } from "react";
 
 function Index() {
   const [resourseType, setResourceType] = useState("posts");
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourseType}`)
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+  }, [resourseType]);
+
   return (
     <div>
       <button onClick={() => setResourceType("posts")}>Post</button>
       <button onClick={() => setResourceType("users")}>Users</button>
       <button onClick={() => setResourceType("comments")}>comments</button>
       <h1>{resourseType}</h1>
+      {items.map((items) => {
+        return <pre>{JSON.stringify(items)}</pre>;
+      })}
     </div>
   );
 }
